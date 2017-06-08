ea899b90b11435e63b2022fb41ebf069ee01282e
Matt Ruttan <matthew.ruttan5@gmail.com>
2017-06-08 17:06:23 -0400

    supposed to have the map feature working, it isn't, error in code, will have to fix

--- asciichan.py
+++ asciichan.py
@@ -1,6 +1,12 @@
 import os
-import jinja2
+import re
+import sys
+import urllib2
+from xml.dom import minidom
+from string import letters
+
 import webapp2
+import jinja2
 
 from google.appengine.ext import db
 
@@ -8,6 +14,11 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 																autoescape = True)
 
+# URLError= ""
+art_key = db.Key.from_path('ASCIIChan', 'arts')
+
+def console(s):
+		sys.stderr.write('%s\n' % s)
 
 class Handler(webapp2.RequestHandler):
 		def write(self, *a, **kw):
@@ -20,28 +31,85 @@ class Handler(webapp2.RequestHandler):
 		def render(self, template, **kw):
 				self.write(self.render_str(template, **kw))
 
+GMAPS_URL = "http://maps.googleapis.com/maps/api/staticmap?size=380x263&sensor=false&"
+def gmaps_img(points):
+		markers = '&'.join('markers=%s,%s' % (p.lat, p.lon) for p in points)
+		return GMAPS_URL + markers
+
+IP_URL = "http://api.hostip.info/?ip="
+def get_coords(ip):
+		ip = "4.2.2.2"
+		ip = "23.24.209.141"
+		url = IP_URL + ip
+		content = None
+		try:
+				content = urllib2.urlopen(url).read()
+		
+		# this doesnt work at all, oh well, this line breaks the code currently
+		# except URLError:
+		# 		return
+		except:
+				return
+
+		if content:
+				d = minidom.parseString(xml)
+				coords = d.getElementsbyTagName("gml:coordinates")
+				if coords and coords[0].childNodes[0].nodeValue:
+						lon, lat = coords[0].childNodes[0].nodeValue.split(',')
+						return db.GeoPt(lat, lon)
+
 class Art(db.Model):
 		title = db.StringProperty(required = True)
 		art = db.TextProperty(required = True)
 		created = db.DateTimeProperty(auto_now_add = True)
+		coords = db.GeoPtProperty()
 
 class MainPage(Handler):
 		def render_front(self, title="", art="", error=""):
 				arts = db.GqlQuery("SELECT * FROM Art "
-														"ORDER BY created DESC ")
+														"WHERE ANCESTOR IS :1 "
+														"ORDER BY created DESC "
+														"LIMIT 10",
+														art_key)
 
-				self.render("front.html", title=title, art=art, error=error, arts=arts)
+				# points = []
+				# for a in arts:
+				# 		if arts.coords:
+				# 				points.append(a.coords)
+
+				#prevent the running of multiple queries
+				arts = list(arts)
+
+				#find which arts have coords
+				points = filter(None, (a.coords for a in arts))
+
+				#if we have any arts coords, make an image url
+				img_url = None
+
+				if points:
+						img_url = gmaps_img(points)
+
+
+
+				self.render("front.html", title=title, art=art, error=error, arts=arts, img_url=img_url)
 
 		def get(self):
-				self.render_front()
+				self.write(self.request.remote_addr)
+				self.write(repr(get_coords(self.request.remote_addr)))
+				return self.render_front()
 
 		def post(self):
 				title = self.request.get("title")
 				art = self.request.get("art")
 
 				if title and art:
-						a = Art(title = title, art = art)
-						a.put()
+						p = Art(parent = art_key, title = title, art = art)
+						#lookup user's coords
+						coords = get_coords(self.request.remote_addr)
+						if coords:
+								p.coords = coords
+
+						p.put()
 
 						self.redirect("/")
 				else:
